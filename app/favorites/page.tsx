2c37c83815f5d37054023f0deda141e51db8123e
Zonda6996 <ifresh3d@gmail.com>
2026-04-08 14:49:28 +0500

    feat: update favorites page

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,6 +1,23 @@
+'use client'
+
+import { useFavorites } from '../shared/hooks/useFavorites'
+import CatCard from '../widgets/CatGallery/CatCard'
 
 const FavoritesPage = () => {
-	return <div>FavoritesPage</div>
+	const { favorites, isFavorite, toggleFavorite } = useFavorites()
+
+	return (
+		<div className='grid grid-cols-2 gap-4 md:gap-12 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3'>
+			{favorites.map(cat => (
+				<CatCard
+					key={cat.id}
+					cat={cat}
+					isFavorite={isFavorite(cat.id)}
+					onToggle={toggleFavorite}
+				/>
+			))}
+		</div>
+	)
 }
 
 export default FavoritesPage
